0ca9aaeeedbac8979c5f549469bfd8a1b1a01f5c
haols <haols@tsintergy.com>
2022-12-15 15:33:22 +0800

    fix: 增加打包配置，采用pnpm，添加peerDependencies

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,12 @@
 export default {
   esm: 'rollup',
   cjs: 'rollup',
+  extraBabelPlugins: [
+    [
+      'transform-remove-console',
+      {
+        exclude: ['error', 'warn'],
+      },
+    ],
+  ],
 };
